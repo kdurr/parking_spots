4ee83e116e5610edc5793d0acee4780428cdec98
kdurr <kristina.l.durr@gmail.com>
2013-09-16 09:04:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParkingCheck::Application.config.secret_key_base = '77d7df682cda2534f3c3ab99a8d40fd4ff09a8b348d7cbd2b87c9004a53728d86e92f8c06407b6f81a564caa847c89db785dff0c9b707be9db70ae627b2f8d75'
